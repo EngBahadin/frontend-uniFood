7103623d185374769a71b2f99c92cc3529cc1558
Bahadin <bahaking341@gmail.com>
2024-11-08 20:08:43 +0300

    .

--- src/app/_components/OrderedFoodCart.tsx
+++ src/app/_components/OrderedFoodCart.tsx
@@ -1,60 +1,80 @@
-'use client'
+"use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { PiAlarmThin } from "react-icons/pi";
+import { toast } from "sonner";
+import ConfirmModal from "./ui/ConfirmModal";
 
-
-interface OrderHistoryCartProps  {
-cartType : string;
-preparationTime ?:string
-preparationStatus?:string
+interface OrderHistoryCartProps {
+  cartType: string;
+  preparationTime?: string;
+  preparationStatus?: string;
 }
+const foods = [
+  { id: 1, name: "pizza", price: 5000, image: "/pizza.png" },
+  {
+    id: 2,
+    name: "  Meat cheese burger",
+    price: 4500,
+    image: "/cheese-burger.png",
+  },
+];
 
+function OrderedFoodCart({ cartType, preparationTime }: OrderHistoryCartProps) {
+  const [currentFoodIndex, setCurrentFoodIndex] = useState(0); // Tracks which food is displayed
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+  // Handle Next and Previous clicks for foods
+  const handleNextFood = () => {
+    if (currentFoodIndex < foods.length - 1) {
+      setCurrentFoodIndex(currentFoodIndex + 1);
+    } else {
+      setCurrentFoodIndex(0); // Loop back to the first food
+    }
+  };
 
+  const handlePreviousFood = () => {
+    if (currentFoodIndex > 0) {
+      setCurrentFoodIndex(currentFoodIndex - 1);
+    } else {
+      setCurrentFoodIndex(foods.length - 1); // Loop back to the last food
+    }
+  };
+  const handleNavigate = () => {
+    toast.success("Order is Confirmed");
+    router.push("/");
+  };
+  const handleClose = () => {
+    setShowModal(false);
+  };
 
-function OrderedFoodCart({cartType,preparationTime}:OrderHistoryCartProps) {
-      const foods = [
-        { id: 1, name: "Biryani", price: 5000, image: "/pizza.png" },
-        { id: 2, name: "Burger", price: 5000, image: "/cheese-burger.png" },
-      ];
-
-      
-      const [currentFoodIndex, setCurrentFoodIndex] = useState(0); // Tracks which food is displayed
-
-
-      // Handle Next and Previous clicks for foods
-      const handleNextFood = () => {
-        if (currentFoodIndex < foods.length - 1) {
-          setCurrentFoodIndex(currentFoodIndex + 1);
-        } else {
-          setCurrentFoodIndex(0); // Loop back to the first food
-        }
-      };
-
-      const handlePreviousFood = () => {
-        if (currentFoodIndex > 0) {
-          setCurrentFoodIndex(currentFoodIndex - 1);
-        } else {
-          setCurrentFoodIndex(foods.length - 1); // Loop back to the last food
-        }
-      };
-
-      // Get the current food from the array
-      const currentFood = foods[currentFoodIndex];
+  // Get the current food from the array
+  const currentFood = foods[currentFoodIndex];
   return (
     <article className="rounded-xl shadow-lg overflow-hidden border-[1px] border-primary-lm ">
       {/* Food Info */}
       <div className="flex justify-between gap-x-14 p-5 bg-pure-white">
         <span className="flex flex-col justify-between gap-y-2">
-          <h2 className="text-body-3-medium">Food ID: #{currentFood.id}</h2>
+          <h2 className="text-body-3-medium">
+            Food ID: #{Math.floor(Math.random() * 1000)}
+          </h2>
           <p className="text-text-2-regular text-gray-100">
-            23 Feb 2025, 10:00 AM
+            {new Date().toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+            , 10:00 AM
           </p>
         </span>
         <span className="flex flex-col items-end gap-y-2">
-          <button className="bg-primary-lm text-pure-white text-text-2-regular py-1 px-3 rounded-lg ">
+          <button
+            onClick={() => cartType === "delivered" && setShowModal(true)}
+            className="bg-primary-lm text-pure-white text-text-2-regular py-1 px-3 rounded-lg "
+          >
             {cartType === "delivered" ? (
               "Re-Order"
             ) : (
@@ -66,7 +86,7 @@ function OrderedFoodCart({cartType,preparationTime}:OrderHistoryCartProps) {
               </>
             )}
           </button>
-          <p className="text-text-2-medium text-gray-100">total: 35000 IQD</p>
+          <p className="text-text-2-medium text-gray-100">total: 9500 IQD</p>
         </span>
       </div>
 
@@ -85,8 +105,8 @@ function OrderedFoodCart({cartType,preparationTime}:OrderHistoryCartProps) {
               className="object-cover size-28"
             />
             <span className="text-white">
-              <h4 className="text-body-4-semiBold">1x Pizza</h4>
-              <p className="text-text-2-regular">5000 IQD</p>
+              <h4 className="text-body-4-semiBold">1x {currentFood.name}</h4>
+              <p className="text-text-2-regular">{currentFood.price} IQD</p>
             </span>
           </div>
           <FaChevronRight
@@ -103,8 +123,11 @@ function OrderedFoodCart({cartType,preparationTime}:OrderHistoryCartProps) {
           ))}
         </div>
       </div>
+      {showModal && (
+        <ConfirmModal onClose={handleClose} onNavigate={handleNavigate} />
+      )}
     </article>
   );
 }
 
-export default OrderedFoodCart
+export default OrderedFoodCart;
--- src/app/order_history/layout.tsx
+++ src/app/order_history/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
@@ -6,13 +6,14 @@ import { ReactNode } from "react";
 const OrderHistory = ({ children }: { children: ReactNode }) => {
   // Static data for a single order's foods
   const pathName = usePathname();
-  const classes = "flex items-center gap-3 text-gray-100 hover:text-primary-lm ";
+  const classes =
+    "flex items-center gap-3 text-gray-100 hover:text-primary-lm ";
   const activeClasses =
     "border-b-[3px] rounded-b-sm border-primary-lm text-primary-lm";
   return (
     <div className="px-16  min-h-screen w-screen">
       <h1 className="md:text-sub-heading-1-semiBold text-sub-heading-2-semiBold text-primary-lm border-l-8 p-2 my-10 w-fit">
-        Profile
+        Order History
       </h1>
       <nav className="pb-14 flex gap-x-10 text-body-4-medium ">
         <Link
